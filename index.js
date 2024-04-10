1c7e45b2b2a9be224143fe4272f0da4f26488857
devjob4475 <thananchai216@gmail.com>
2024-04-10 19:24:40 +0700

    test test seque

--- index.js
+++ index.js
@@ -3,21 +3,21 @@ const app = express();
 const { Sequelize } = require("sequelize");
 const { DataTypes } = require("sequelize");
 
-// const sequelize = new Sequelize(
-//   "verceldb", // ชื่อฐานข้อมูล
-//   "default", // ชื่อผู้ใช้
-//   "TvpP28uVGeXk", // รหัสผ่าน
-//   {
-//     host: "ep-bold-wave-a15og2a1.ap-southeast-1.aws.neon.tech",
-//     dialect: "postgres",
-//     dialectOptions: {
-//       ssl: {
-//         require: true,
-//         rejectUnauthorized: false,
-//       },
-//     },
-//   }
-// );
+const sequelize = new Sequelize(
+  "verceldb", // ชื่อฐานข้อมูล
+  "default", // ชื่อผู้ใช้
+  "TvpP28uVGeXk", // รหัสผ่าน
+  {
+    host: "ep-bold-wave-a15og2a1.ap-southeast-1.aws.neon.tech",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  }
+);
 
 // const User = sequelize.define("User", {
 //     id: {

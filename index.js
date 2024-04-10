d18207a8d3aff524e6c3cd9321ed5940328f0b34
devjob4475 <thananchai216@gmail.com>
2024-04-10 17:30:51 +0700

    post user

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 const express = require("express");
 const app = express();
+app.use(express.json()); // Add this line to parse JSON requests
 
-app.get("/users", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+app.post("/users", (req, res) => {
+    // รับข้อมูล JSON ที่ส่งมาจาก client
+    const { email, firstname, lastname } = req.body;
+  
+    // สร้าง JSON object ใหม่เพื่อคืนให้ client
+    const userData = {
+      email,
+      firstname,
+      lastname,
+    };
+  
+    // คืนข้อมูล JSON ที่ได้รับ
+    res.json(userData);
+  });
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 

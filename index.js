12f909dc08dbf7fcdbee0b856c57e58d2af129cf
devjob4475 <thananchai216@gmail.com>
2024-04-10 17:26:58 +0700

    get user

--- index.js
+++ index.js
@@ -1,19 +1,7 @@
 const express = require("express");
 const app = express();
-const User = require("./models/User");
 
-app.use(express.json());
-
-app.post("/users", async (req, res) => {
-  try {
-    const { email, firstname, lastname } = req.body;
-    const newUser = await User.create({ email, firstname, lastname });
-    res.status(201).json(newUser);
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: "Failed to create user." });
-  }
-});
+app.get("/users", (req, res) => res.send("Express on Vercel"));
 
 app.listen(3000, () => console.log("Server ready on port 3000."));
 

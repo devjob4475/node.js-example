be9792cbd2edb35363bb7e560b8d751b08f5b156
devjob4475 <thananchai216@gmail.com>
2024-04-10 20:19:45 +0700

    add book api2

--- database.js
+++ database.js
@@ -1,29 +1,12 @@
-const { Pool } = require('pg');
-
+const { Pool } = require('pg')
+ 
 const pool = new Pool({
-  connectionString: process.env.POSTGRES_URL,
-});
+  connectionString: process.env.POSTGRES_URL + "?sslmode=require",
+})
 
 pool.connect((err) => {
-  if (err) throw err;
-  console.log("Connected to PostgreSQL successfully!");
-  createBooksTable(); // เรียกใช้ฟังก์ชันสร้างตาราง books เมื่อเชื่อมต่อฐานข้อมูลสำเร็จ
-});
+    if (err) throw err
+    console.log("Connect to PostgreSQL successfully!")
+})
 
-async function createBooksTable() {
-  try {
-    const createTableQuery = `
-      CREATE TABLE IF NOT EXISTS books (
-        book_id SERIAL PRIMARY KEY,
-        name VARCHAR(255) NOT NULL,
-        price NUMERIC(10, 2) NOT NULL
-      );
-    `;
-    await pool.query(createTableQuery);
-    console.log("Table 'books' created or already exists.");
-  } catch (err) {
-    console.error("Error creating table:", err);
-  }
-}
-
-module.exports = pool;
+module.exports = pool

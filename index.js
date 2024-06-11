1ca771442f8bbeb188961faaa1d9c47f6405cfd3
devjob1234 <thananchai216@gmail.com>
2024-06-11 23:45:50 +0700

    add api

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
-const express = require("express")
-const app = express()
+const express = require("express");
+const app = express();
+const axios = require('axios');
+require('dotenv').config();
 
-require('dotenv').config()
+app.use(express.json());
 
-app.use(express.json())
+const bookRouter = require('./routes/book.router');
+app.use("/api/v1/books", bookRouter);
 
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
 
-const bookRouter = require('./routes/book.router')
+let config = {
+  method: 'get',
+  maxBodyLength: Infinity,
+  url: 'https://inwiptv.com/Ajax_list.php?id=live&fid=4&type=1&Cookie=cuser=joft70302; cpass=123456789; PHPSESSID=e71d9671300f6c0773da7f3e754e6716; __cflb=02DiuJFQwB4GELYEJyxVz6Cd7qouHJm1mE2CTpuwqQP6K',
+  headers: { 
+    'Cookie': 'PHPSESSID=df7170500b7ca6b33a6b3433f5e87439; __cflb=0H28vwuuPYDN6Z6M5v83LgEY2qZJRyXtGjCYfQkqKcX'
+  }
+};
 
-app.use("/api/v1/books", bookRouter)
+const fetchApiData = () => {
+  axios.request(config)
+  .then((response) => {
+    console.log(JSON.stringify(response.data));
+  })
+  .catch((error) => {
+    console.error(error);
+  });
+};
 
-app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`))
+// Fetch the API every 2 seconds
+setInterval(fetchApiData, 2000);
